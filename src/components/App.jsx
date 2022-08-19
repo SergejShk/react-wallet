9afb28ece590ae2f2fed683df25a9bbc0c03fe16
SergejShk <sshkurupij11@gmail.com>
2022-08-19 18:35:05 +0300

    fix name folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { getToken } from 'redux/auth/authSelector';
 import { getIncomesCategories } from 'redux/categories/categoriesOperations';
 
-const MainPage = lazy(() => import('./MainPage/MainPage'));
+const MainPage = lazy(() => import('./mainPage/MainPage'));
 const TransactionHistoryPage = lazy(() =>
   import('./TransactionHistoryPage/TransactionHistoryPage')
 );
